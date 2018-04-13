5a5a583b92760910adfaa03e18686c7889dfdc38
Jack Gumoes <j.gumoes@gmail.com>
2018-04-14 00:32:51 +0100

    Tweaked some stuff in brute force and interpolation. About to split plot_three()

--- mathing the data.py
+++ mathing the data.py
@@ -19,6 +19,11 @@ path=r"C:\Users\Jack\Documents\Uni\GDP\Drop Tests\\"
 1100 g start and length is [201, 633, 1086], 71
 
 1667 g start and length is [179, 655, 1069], 71
+
+usage procedure:
+    raw_data() to import the data
+    plot_angle() to see where to trim the data (only keep the sharp accelerations)
+    
 """
 
 theta = 360/30 # degrees per pulse
@@ -44,7 +49,6 @@ def plot_angle(data, theta=360/90):
     """plots the angle against time. doesn't account for reverse motion"""
     times = data/10**6
     times-=times[0]
-    #theta = 360/90. # angle between each edge
     angles = np.linspace(0, theta*len(times), len(times), endpoint=False)
     vel = theta/(times[1:]-times[:-1])
     filt_vel = signal.savgol_filter(vel, 11, 3)
@@ -115,7 +119,7 @@ def plot_three(times, pc=False, p=False):
     c_data = np.vstack([c_t[:length], c_A[:length]])
     return a_data, b_data, c_data
     
-def combine_three(data, D1, D2, sav=[13, 2], p=False):
+def combine_three(data, D1, D2, sav=[13, 2], p=False, alpha=True):
     """recombines the three data streams from plot_three() into 1 stream"""
     a_data, b_data, c_data = data
     a_t, a_A = a_data
@@ -124,8 +128,6 @@ def combine_three(data, D1, D2, sav=[13, 2], p=False):
     lens = [len(a_A), len(c_A), len(b_A)]
     length = min(lens)
     
-    #D1, D2 = find_D(data, [a_A, b_A, c_A])
-    #D2 = find_D(data, [a_A, b_A, c_A])[1]
     A = np.stack([a_A[:length], b_A[:length]+D1+theta/3., c_A[:length]+D2+2*theta/3.]).flatten('F')
     T = np.stack([a_t, b_t, c_t]).flatten('F')
     W = (A[1:]-A[:-1])/(T[1:]-T[:-1])
@@ -133,17 +135,25 @@ def combine_three(data, D1, D2, sav=[13, 2], p=False):
     if p is True:
         plt.plot(T[:-1], W)
     #filt_W = signal.savgol_filter(W, sav[0], sav[1], mode="nearest")
-    filt_times, filt_W = interp(data,W=True)
+    #filt_times, filt_W = interp(data,W=True)
+    data[1][1]+=D1
+    data[2][1]+=D2
+    filt_times, filt_a = interp(data, alpha=alpha)
+    l = np.argwhere(T==filt_times[0])[0][0]
+    h = np.argwhere(T==filt_times[-1])[0][0]+1
+    W = W[l: h]
+    a = (W[1:]-W[:-1])/(filt_times[1:]-filt_times[:-1])
     if p is True:
-        plt.plot(T[:-1], W)
-        plt.plot(filt_times, filt_W)
+        plt.plot(filt_times, filt_a)
+        #plt.plot(filt_times[:-1], a)
+        #plt.plot(filt_times[:-1], filt_a)
     #print(D1,"\t", D2, "\t", np.sum((W-filt_W)**2))
-    return np.sum((W[3:-3]-filt_W)**2)
-    A = np.stack([a_A[:length], b_A[:length]+theta/3., c_A[:length]+2*theta/3.]).flatten('F')
-    W = (A[1:]-A[:-1])/(T[1:]-T[:-1])
-    #plt.plot(T[:-1], W)
+    #return np.sum((W[3:-3]-filt_W)**2)
+    if alpha is True:
+        return np.sum((a-filt_a)**2)
+    return T, A, np.sum((W-filt_a)**2)
     
-def interp(dataset, W=True, p=False):
+def interp(dataset, alpha=True, W=True, p=False):
     """combines the sensor data through interpolation\n
     dataset should be a list of numpy arrays, where each array has the form 
     [[times], [angles]]"""
@@ -159,53 +169,20 @@ def interp(dataset, W=True, p=False):
     for i in dataset:
         average += (interpolate.interp1d(i[0], i[1])(newtimes))
     average = average/n
+    w = (average[1:]-average[:-1])/(newtimes[1:]-newtimes[:-1])
+    w_times = newtimes[:-1]
+    a = (w[1:]-w[:-1])/(w_times[1:]-w_times[:-1])
+    if alpha is True:
+        if p is True:
+            plt.plot(w_times[:-1], a)
+        return w_times, a
     if W is True:
-        w = (average[1:]-average[:-1])/(newtimes[1:]-newtimes[:-1])
         if p is True:
             plt.plot(newtimes[:-1], w)
         return newtimes[:-1], w
     if p is True:
         plt.plot(newtimes, average)
     return newtimes, average
-    
-
-
-def find_D(times, angles):
-    """finds the d (the pulse offset value) by minimizing n_peaks.\n
-    this function is for re-combining the 3 sensors into one dataset"""
-    theta = 2*360/90
-    a_A, b_A, c_A = angles
-    lens = [len(a_A), len(c_A), len(b_A)]
-    length = min(lens)
-    #angles = np.stack([a_A[:length], b_A[:length]+theta/3., c_A[:length]+2*theta/3.]).flatten('F')
-    #times = times[:len(angles)]
-    angles = np.vstack((a_A[:length], b_A[:length], c_A[:length]))
-    angles += np.array([[0], [1], [2]])*theta/3
-    angles = angles.flatten()
-    params = Parameters()
-    params.add('D1', value=0) #, min=-theta, max=theta)
-    params.add('D2', value=0) #, min=-theta, max=theta)
-    data = np.concatenate([times, angles])
-    out = minimize(square_filt_D, params, args=([data]))
-    return out.params.valuesdict()['D1'], out.params.valuesdict()['D2']
-
-def square_filt_D(param, data):
-    """Returns the square of the difference between unfiltered and filtered data.\n
-    param is the lmfit parameter for delta \n
-    dummy is an empty variable. Has to be there for the code to work.\n
-    this function is for re-combining the 3 sensors into one dataset"""
-    sav=[13, 2]
-    data = data.reshape((2, int(len(data)/2)))
-    times, angles = data
-    D1 = param.valuesdict()['D1']
-    D2 = param.valuesdict()['D2']
-    A = angles
-    #A = (angles.reshape((3, int(len(angles))/3)) + np.array([[0], [D1], [D2]])).flatten()
-    A += np.append(np.tile([0, D1, D2], int(len(A)/3)), [0, D1, D2][:len(A)%3])
-    T = times[:len(A)]
-    W = (A[1:]-A[:-1])/(T[1:]-T[:-1])
-    filt_W = signal.savgol_filter(W, sav[0], sav[1])
-    return np.sum((W-filt_W)**2), np.sum((W-filt_W)**2)
 
 
 def find_d(times):
@@ -231,7 +208,6 @@ def square_filt_d(param, t, dummy, sav=[11, 2]):
     A = np.linspace(0, theta*len(t), len(t), endpoint=False)
     A += da*theta*np.append(np.tile([1, -1], int(len(t)/2)), [1][:len(t)%2])
     W = (A[1:]-A[:-1])/(t[1:]-t[:-1])
-    alpha = (W[1:]-W[:-1])/(t[2:]-t[:-2])
     filt_W = signal.savgol_filter(W, sav[0], sav[1])
     return np.sum((W-filt_W)**2)
 
@@ -259,19 +235,10 @@ def brute_force(data, tol=360/30, it=None, res=10**6):
             while j<N:
                 r = combine_three(plot_three(data), D1_list[i], D2_list[j])
                 res[i, j] +=  r
-                #print(D1_list[i], "\t", D2_list[j], "\t", r)
                 j+=1
             i+=1
-            #min_pos = res.argmin(axis=1)
-            #min_vals = np.zeros(len(min_pos))
-            #for i in range(len(min_pos)):
-            #    min_vals[i] = res[i][min_pos[0]]
-            #mins = res
         min_value = res.min()
-        #print(min_value)
         min_pos = np.argwhere((res==min_value))[0]
-        #print(res)
-        #print(min_pos)
         D1_l = D1_list[min_pos[0]-1]
         D1_h = D1_list[min_pos[0]+1]
         D2_l = D2_list[min_pos[1]-1]
