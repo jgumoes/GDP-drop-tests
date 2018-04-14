bc433e0c4e6ad0702eea9804d62eb3f77e1023d9
Jack Gumoes <j.gumoes@gmail.com>
2018-04-14 11:58:43 +0100

    Created a script to do the actual data processing. Should help keep things tidy

---
+++ data processing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 11:28:06 2018
+
+@author: Jack
+"""
+
+from "mathing the data.py" import *
+
+path=r"C:\Users\Jack\Documents\Uni\GDP\Drop Tests" + "\\"
+
+print(raw_data(path+"free spinning.txt"))
