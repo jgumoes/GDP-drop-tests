f97943b030f523a743b159e708546e1c34edde54
Jack Gumoes <j.gumoes@gmail.com>
2018-05-06 11:34:49 +0100

    Close to finished. Going to remove the euler stuff

--- data processing.py
+++ data processing.py
@@ -17,7 +17,7 @@ from scipy import signal
 path=r"C:\Users\Jack\Documents\Uni\GDP\Drop Tests" + "\\"
 file = r"C:\Users\Jack\Documents\Uni\GDP\Drop Tests\0\0.npy"
 
-J_A = None
+#J_A = None
 #print(raw_data(path+"free spinning.txt"))
 
 # =============================================================================
@@ -67,9 +67,7 @@ def fit_theta_free(data):
     params = lm.Parameters()
     params.add("k1", value = 0.5, min=0)
     params.add("k2", value = 0.5, min=0)
-    global J_A
-    if J_A is None:
-        make_ja()
+    J_A = make_ja()
     minner = lm.Minimizer(opt_theta_free, params, fcn_args=(t, th, J_A, w0))
     return minner.minimize()
     
@@ -157,18 +155,21 @@ def invert(t, w, plot=False):
         plt.plot(times, w_inv)
     return times, w_inv
 
+@jit(cache=True)
 def make_m(weight=0, accel=0):
     """Returns to total effective mass of the experiment"""
     w_motor = 1.6 #double check this
     w_wheel = 0.620 #accurate
-    return w_motor + w_wheel + weight*(1-accel/9.81)
+    g = 9.809915
+    return w_motor + w_wheel + weight*(1-accel/g)
 
+@jit(cache=True)
 def make_ja():
     """calculates the moment of inertia for the testing wheel.\n
-    in a function because I don't like all the variable it was making.\n
+    in a function because I don't like all the variables it was making.\n
     d_ is the densit in kg/m**2; _o is the outer disks, _i is the inner disk,
     _b are the bolts, and _h are the holes"""
-    global J_A
+   # global J_A
     d_o = 0.138/(((0.29996/2))**2 - 20*0.002**2 - 0.04**2)
     d_i = 0.283/((0.288/2)**2 - 20*0.002**2 - 0.04**2)
     J_h = 20*(0.002**4) + 28*(0.002**2)*(0.137**2) + 12*(0.002**2)*(0.059**2) + (0.080)**4
@@ -176,11 +177,102 @@ def make_ja():
     J_i = d_i/2 * ((0.288/2)**4 - J_h)
     J_b = 0.061/40 * (14*(0.137**2) + 6*(0.059**2))
     J_A = J_o*2 + J_i + J_b     #moment of inertia for the testing wheel
+    return J_A
 
-make_ja()
+@jit(cache=True)
+def Tf(k1, k2, u2, u3, u4, m, w):
+    """returns the friction for a given velocity and mass"""
+    return m*k2*np.sqrt((k1*w)**2 + 2*k1*w)/k1 + u2*w + u3 + u4*m
 
+@jit(cache=True)
+def a_free(k1, k2, u2, u3, u4, m, w):
+    """returns the acceleration for a given velocity for the free-spinning tests"""
+    j = make_ja()
+    return -Tf(k1, k2, u2, u3, u4, m, w)/j
 
+@jit(cache=True)
+def opt_free(params, files, p=False):
+    k1, k2, u2, u3, u4, cut1, cut2 = params
+    err = 0
+    m = make_m()
+    for i in files:
+        data = np.load(i)
+        data[1] *= 2*np.pi/360
+        w, a = butt_2(cut1, cut2, data, wend=0, plot=False)
+        am = a_free(k1, k2, u2, u3, u4, m, w)
+        err += np.average((am[:-1]-a)**2)
+        if p is True:
+            plt.plot(data[0][:len(a)], a)
+            plt.plot(data[0][:len(am)], am)
+    return err
 
+@jit(cache=True)
+def a_mass(k1, k2, u2, u3, u4, weight, w, a):
+    """returns the acceleration for a given velocity for the drop tests"""
+    r = 0.288/2
+    j = make_ja()
+    m = make_m(weight, a/r)
+    tf = Tf(k1, k2, u2, u3, u4, m, w[:-1])
+    return tf/(m*(r**2) - j)
+
+@jit(cache=True)
+def opt_mass(params, files, p=False):
+    k1, k2, u2, u3, u4, cut1, cut2 = params
+    err = 0
+    for i in files:
+        data = np.load(i)
+        data[1] *= 2*np.pi/360
+        Ts = 10**-6
+        if data[0][1] >= 0.9:
+            Ts *= 10**6
+        data = resample(data, Ts)
+        w, a = butt_2(cut1, cut2, data, wend=None, plot=False)
+        weight = int(i.split("\\")[-2])/1000
+        am = a_mass(k1, k2, u2, u3, u4, weight, w, a)
+        err += np.average((am-a)**2)
+        if p is True:
+            plt.plot(data[0][:len(a)], a)
+            plt.plot(data[0][:len(am)], am)
+    return err
+
+@jit(cache=True)
+def opt_all_w(params):
+    k1, k2, u2, u3, u4, cut1, cut2, cut3, cut4 = params
+    f_files = ['C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\14.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\15.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\16.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\17.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\18.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\0.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\1.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\2.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\3.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\4.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\5.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\6.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\7.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\8.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\9.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\10.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\11.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\12.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\0\\13.npy']
+    m_files = ['C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\575\\2.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\575\\0.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\575\\1.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1100\\2.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1100\\0.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1100\\1.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1667\\0.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1667\\1.npy', 'C:\\Users\\Jack\\Documents\\Uni\\GDP\\Drop Tests\\1667\\2.npy']
+    f_params = [k1, k2, u2, u3, u4, cut1, cut2]
+    m_params = [k1, k2, u2, u3, u4, cut3, cut4]
+    err = opt_free(f_params, f_files) + opt_mass(m_params, m_files)
+    return err
+
+@jit(parallel=True)
+def opt_all():
+    """performs optimisations on all the files"""
+    params = [1, 1, 1, 1, 1, 20, 20, 20, 20]
+    bounds = [(1e-06, None), (0, None), (0, None), (0, None), (0, None), (1, 30), (1, 30), (1, 30), (1, 30)]
+    res = op.minimize(opt_all_w, params, bounds=bounds, tol=10**-11)
+    return res
+
+@jit(cache=True)
+def resample(data, Ts=None):
+    """resamples the data so that the sampling period is constant.\n
+    if Ts is None, it tries to work out the lowest possible Ts that won't
+    undersample the data"""
+    t = data[0]
+    d = data[1]
+    interp = interpolate.interp1d(t, d)
+    if Ts is None:
+        dt = t[1:]-t[:-1]
+        Ts = min(dt)
+    N = 1 + (t[-1]-t[0])/Ts
+    if N != int(N):
+        N += 1
+    N = int(N)
+    t_new = np.linspace(t[0], t[-1], N)
+    d_new = interp(t_new)
+    return np.array([t_new, d_new])
+
+@jit(cache=True)
 def butt_2(cut1, cut2, data, order=1, plot=True, wend=0):
     """finds the 2nd derivative of the data, and smooths with a butterworth filter.
     20 and 20 are good first estimates for cut1 and cut2."""
@@ -200,9 +292,9 @@ def butt_2(cut1, cut2, data, order=1, plot=True, wend=0):
         plt.plot(t[:len(w)], w)
     filt = butt([t[:len(w)], w], cut2, plot=plot)
     if plot is not True:
-        return filt
+        return w, filt
 
-
+@jit(cache=True)
 def butt(data, cut, order=1, plot=True, end=None, opt=False):
     """finds the derivative of data, and smooths with a butterworth filter"""
     t = data[0]
@@ -235,3 +327,19 @@ def butt_err(init, u, cut, order):
     filt = signal.filtfilt(b, a, uf-init) + init
     filt = filt[10:]
     return np.sum(np.abs(filt[:10]-u[:10]))
+
+def plot_res(params, files):
+    k1, k2, u2, u3, u4, cut1, cut2 = params
+    for i in files:
+        data = np.load(i)
+        data[1] *= 2*np.pi/360
+        Ts = 10**-6
+        if data[0][1] >= 0.9:
+            Ts *= 10**6
+        weight = int(i.split("\\")[-2])/1000
+        if weight != 0:
+            data = resample(data, Ts)
+        w, a = butt_2(cut1, cut2, data, wend=None, plot=False)
+        am = a_mass(k1, k2, u2, u3, u4, weight, w, a)
+        plt.plot(data[0][:len(a)], a)
+        plt.plot(data[0][:len(am)], am)
